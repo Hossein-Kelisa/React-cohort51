e5e8af0f036a7b28b715e1cc9d0325e18089a003
Hossein <Hossein.kelisa@gmail.com>
2025-04-30 16:10:00 +0200

    finish assignment w3

--- week3/project/ecommerce/src/context/FavoriteContext.jsx
+++ week3/project/ecommerce/src/context/FavoriteContext.jsx
@@ -1,7 +1,5 @@
-import React, { useState, createContext } from "react";
-
-// Create the FavoriteContext
-export const FavoriteContext = createContext();
+import React, { useState } from "react";
+import { FavoriteContext } from "./FavoriteContextInstance";
 
 export const FavoriteProvider = ({ children }) => {
   const [favorites, setFavorites] = useState([]);
